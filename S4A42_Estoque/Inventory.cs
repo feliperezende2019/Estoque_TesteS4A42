ff18210e4fbc34355f03e548152f17b3a6b0f52f
Felipe Rezende <feliperezende2019@hotmail.com>
2021-09-29 00:12:10 -0300

    Criando pequeno programa de estoque

---
+++ S4A42_Estoque/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace S4A42_Estoque
+{
+    class Inventory
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Price * Quantity;
+        }
+
+        public void AdicionarProdutos(int quantidy)
+        {
+            Quantity = Quantity + quantidy;
+        }
+
+        public void RemoverProdutor(int quantidy)
+        {
+            Quantity = Quantity - quantidy;
+        }
+
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ S4A42_Estoque/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+namespace S4A42_Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            Inventory product = new Inventory();
+
+            // -----------------
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            product.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            product.Price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            product.Quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + product);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidy = int.Parse(Console.ReadLine());
+            product.AdicionarProdutos(quantidy);
+            Console.WriteLine();
+
+            Console.WriteLine("Produtos atualizados: " + product);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidy = int.Parse(Console.ReadLine());
+            product.RemoverProdutor(quantidy);
+            Console.WriteLine();
+
+            Console.WriteLine("Produtos atualizados: " + product);
+        }
+    }
+}
